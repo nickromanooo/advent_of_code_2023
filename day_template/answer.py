503af79b59a9ab36e65e73e1ed97080ef2d62ff4
nick.romano <nickromanooo@gmail.com>
2023-12-01 17:09:11 -0500

    template

---
+++ day_template/answer.py
@@ -0,0 +1,15 @@
+# https://adventofcode.com/2022/day/#
+
+def part_one(file):
+    f = open(file,'r')
+    return
+
+
+def part_two(file):
+    f = open(file,'r')
+    return 
+
+print(f"Part one: {part_one('input.txt')}")
+print(f"Part one test: {part_one('test_input.txt')}")
+print(f"Part two: {part_two('input.txt')}")
+print(f"Part two test: {part_two('text_input.txt')}")
