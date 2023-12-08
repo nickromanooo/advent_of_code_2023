500276586405d07cc2f018c7b8f98180549751a8
nick.romano <nickromanooo@gmail.com>
2023-12-08 12:48:43 -0500

    Part two, thank you math.lcm

--- day_8/answer.py
+++ day_8/answer.py
@@ -1,6 +1,6 @@
 # https://adventofcode.com/2023/day/8
 import os 
-
+import math
 def part_one(file):
     f = open(file,'r')
     instructions, values = [line.strip() for line in f.read().split('\n\n')]
@@ -23,10 +23,32 @@ def part_one(file):
 
 def part_two(file):
     f = open(file,'r')
-    return
+    instructions, values = [line.strip() for line in f.read().split('\n\n')]
+     # 0 is left, 1 is right, as instructions are stored as pairs X => (L,R)
+    instructions = [0 if x == 'L' else 1 for x in [*instructions]]
+    values = [line.strip().split(' = ') for line in values.split('\n')]
+    values = {
+        line[0]:line[1].strip(' ()').split(', ')
+        for line in values
+    }
+    current_values = [value for value in values.keys() if value[-1] == 'A']
+    count = 0
+    found_intervals = []
+    for current_value in current_values:
+        count = 0
+        while current_value[-1] != 'Z':
+            current_instruction = instructions[count % len(instructions)]
+            if count == 0:
+                assert(current_instruction == 0)
+            count+=1
+            current_value = values[current_value][current_instruction]
+        found_intervals.append(count)
+    return math.lcm(*found_intervals)
+
+
 
 dirname, _ = os.path.split(os.path.abspath(__file__))
-print(f"Part one test: {part_one(dirname + '/test_input.txt')}")
-print(f"Part one: {part_one(dirname + '/input.txt')}")
-# print(f"Part two test: {part_two(dirname + '/test_input.txt')}")
-# print(f"Part two: {part_two(dirname + '/input.txt')}")
+# print(f"Part one test: {part_one(dirname + '/test_input.txt')}")
+# print(f"Part one: {part_one(dirname + '/input.txt')}")
+print(f"Part two test: {part_two(dirname + '/test_input.txt')}")
+print(f"Part two: {part_two(dirname + '/input.txt')}")
